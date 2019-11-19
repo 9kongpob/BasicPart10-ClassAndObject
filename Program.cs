e5c4b20ab693d3f343fbaf64f5d3621add763a48
9kongpob <k.kunchanuan@hotmail.com>
2019-11-19 18:08:34 +0700

    Add Coding -Class : Square -Class : Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace BasicPart10_ClassAndObject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Square a = new Square();
+            a.name = "Paper";
+            a.SetDimension(8, 11);
+
+            Square b = new Square();
+            b.name = "KONGPOP";
+            b.SetDimension(16, 9);
+
+            Console.WriteLine("Object " + a.name + " has area = " + a.CalculateArea());
+            Console.WriteLine("Object " + b.name + " has area = " + b.CalculateArea());
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPart10_ClassAndObject
+{
+    class Square
+    {
+        public string name;
+        private int width;
+        private int height;
+
+        public void SetDimension(int w,int h) 
+        {
+            width = w;
+            height = h;
+        }
+        public int CalculateArea()
+        {
+            return width * height;
+        }
+
+    }
+}
